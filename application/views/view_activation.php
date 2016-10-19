fb564fde6fc658ebcb92a5cb721cfa1d2e37d2d3
edu <edu@serverpark.net.my>
2016-10-19 10:24:40 +0700

    update ui

--- application/views/view_activation.php
+++ application/views/view_activation.php
@@ -3,15 +3,17 @@
 	<div class="row">
 		<div class="col-lg-4 col-lg-offset-4">
 		<br>
-			<h2>Aktifasi Akun Anda</h2>
-			<hr>
+			
+			
 			<br />
 			
 		</div>		
 	</div>
 	<div class="row">
-		<div class="col-lg-4 col-lg-offset-4 register">
+		<div class="col-lg-4 col-lg-offset-4 register" style="box-shadow:0 10px 16px 0 rgba(0,0,0,0.2),0 6px 20px 0 rgba(0,0,0,0.19);">
 			<?php echo form_open('security/activate'); ?>
+			<h2>Aktifasi Akun Anda</h2>
+			<br>
 				<?php echo validation_errors('<p class="alert alert-danger">');?>
 				<div class="form-group">
 					<label>Hai, <?php echo $full_name?></label>
@@ -29,6 +31,7 @@
 				<input type="hidden" id="h_full_name" name="h_full_name" value="<?php echo $full_name?>" />
 				<input type="hidden" id="h_dynamic_codee" name="h_dynamic_code" value="<?php echo $dynamic_code?>" />
 			<?php echo form_close();?>
+			<br>
 		</div>
 	</div>
 </div>	
